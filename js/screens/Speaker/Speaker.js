efe55bc2fd60696bbe0a6859ba46905e5493844c
Sophie Virtue <sophie.virtue@gmail.com>
2019-03-03 14:47:34 -0800

    Finish styling Speaker screen

--- js/screens/Speaker/Speaker.js
+++ js/screens/Speaker/Speaker.js
@@ -3,40 +3,52 @@ import {
   View,
   Text,
   Image,
-  Button,
+  TouchableOpacity,
   Linking,
-  TouchableHighlight
+  TouchableHighlight,
+  ScrollView
 } from "react-native";
-import Icon from 'react-native-vector-icons/Ionicons';
+import Icon from "react-native-vector-icons/Ionicons";
+import LinearGradient from 'react-native-linear-gradient'
 import styles from "./styles";
 
 export default class Speaker extends Component {
   state = { modalVisable: false };
   render() {
     return (
-      <View>
-        <View style={{ paddingTop: 40 }}>
-          <TouchableHighlight
-            onPress={() => {
-              this.props.navigation.goBack();
-            }}
-          >
-            <Icon name="ios-close" color="#000" size={60} />
-          </TouchableHighlight>
+      <View style={styles.container}>
+        <View style={styles.flexSpeaker}>
+          <View style={{ paddingTop: 40 }}>
+            <TouchableHighlight
+              onPress={() => {
+                this.props.navigation.goBack();
+              }}
+            >
+              <Icon name="ios-close" style={styles.x} size={50} />
+            </TouchableHighlight>
+          </View>
+          <Text style={styles.heading}>About the Speaker</Text>
         </View>
-        <Text>About the Speaker</Text>
-        <Image
-          style={{ width: 50, height: 50 }}
-          source={{ uri: this.props.image }}
-        />
-        <Text>{this.props.name}</Text>
-        <Text>{this.props.bio}</Text>
-        <Button
-          onPress={() => Linking.openURL(`${this.props.url}`)}
-          title="Learn More on Wikipedia"
-          color="#841584"
-          accessibilityLabel="Learn more about this speaker on Wikipedia"
-        />
+        <ScrollView style={styles.infoSection}>
+          <Image style={styles.image} source={{ uri: this.props.image }} />
+          <Text style={styles.speakerName}>{this.props.name}</Text>
+          <Text style={styles.bio}>{this.props.bio}</Text>
+          <View>
+          <TouchableOpacity
+            onPress={() => Linking.openURL(`${this.props.url}`)}
+            style={styles.button}
+          >
+            <LinearGradient
+              colors={["#9963ea", "#8797D6"]}
+              start={{ x: 0.0, y: 1.0 }}
+              end={{ x: 1.0, y: 0.0 }}
+              style={styles.rounded}
+            >
+              <Text style={styles.buttonText}>Read More on Wikipedia</Text>
+            </LinearGradient>
+          </TouchableOpacity>
+          </View>
+        </ScrollView>
       </View>
     );
   }
--- js/screens/Speaker/styles.js
+++ js/screens/Speaker/styles.js
@@ -1,7 +1,71 @@
-import { StyleSheet } from 'react-native'
+import { StyleSheet } from "react-native";
+import { colors, fonts } from "../../config/styles";
 
 const styles = StyleSheet.create({
-    
-  });
+  container: {
+    backgroundColor: colors.black,
+    height: "100%",
+    paddingLeft: 10,
+    paddingRight: 10
+  },
+  x: {
+    color: colors.white,
+    paddingLeft: 3
+  },
+  heading: {
+    color: colors.white,
+    paddingLeft: 60,
+    paddingTop: 55,
+    paddingBottom: 35,
+    fontSize: 20,
+    fontFamily: fonts.regular
+  },
+  flexSpeaker: {
+    flexDirection: "row"
+  },
+  infoSection: {
+    backgroundColor: colors.white,
+    padding: 15,
+    maxHeight: 645,
+    borderRadius: 10,
+    paddingTop: 25,
+    // paddingBottom: 80
+  },
+  image: {
+    width: 120,
+    height: 120,
+    borderRadius: 60,
+    alignSelf: "center"
+  },
+  speakerName: {
+    paddingTop: 20,
+    paddingBottom: 20,
+    alignSelf: "center",
+    fontFamily: fonts.regular,
+    fontSize: 26
+  },
+  bio: {
+    fontFamily: fonts.light,
+    paddingBottom: 30,
+    fontSize: 19,
+    lineHeight: 30
+  },
+  buttonText: {
+    alignSelf: 'center',
+    fontFamily: fonts.regular,
+    fontSize: 18,
+    color: colors.white,
+    paddingTop: 13,
+    paddingBottom: 13,
+  }, 
+  button: {
+    width: '80%',
+    alignSelf: 'center',
+    paddingBottom: 60
+  },
+  rounded: {
+    borderRadius: 25
+  }
+});
 
-  export default styles;
+export default styles;
